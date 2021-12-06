2df46ded6f458fde69e89440ab98c45b03a57a28
DJ Lee <dennisjlee@gmail.com>
2021-12-06 00:12:54 -0500

    2021 day 6

---
+++ 2021/day6.py
@@ -0,0 +1,42 @@
+import sys
+from collections import defaultdict
+
+
+def main():
+    with open(sys.argv[1]) as f:
+        orig_fish = [int(w) for w in f.readline().strip().split(',')]
+
+    # part 1
+    fish = orig_fish
+    for _ in range(80):
+        new_fish = []
+        for f in fish:
+            if f == 0:
+                new_fish.append(6)
+                new_fish.append(8)
+            else:
+                new_fish.append(f - 1)
+        fish = new_fish
+
+    print(len(fish))
+
+    # part 2
+    fish_counts = defaultdict(int)
+    for f in orig_fish:
+        fish_counts[f] += 1
+
+    for _ in range(256):
+        new_fish_counts = defaultdict(int)
+        for f, count in fish_counts.items():
+            if f == 0:
+                new_fish_counts[6] += count
+                new_fish_counts[8] += count
+            else:
+                new_fish_counts[f-1] += count
+        fish_counts = new_fish_counts
+
+    print(sum(fish_counts.values()))
+
+
+if __name__ == '__main__':
+    main()
