839ff921231ebffd1beef88cda2ac47c7343fb86
DJ Lee <dennisjlee@gmail.com>
2021-12-12 00:32:49 -0500

    2021 day12

---
+++ 2021/day12.py
@@ -0,0 +1,91 @@
+import sys
+from collections import namedtuple, Counter
+import math
+
+
+class Cave:
+    def __init__(self, name: str):
+        self.name = name
+        self.small = name.islower()
+        self.neighbors = []
+        self.max_visits = 1 if self.small else math.inf
+
+
+State = namedtuple('State', ['cave', 'visited', 'path'])
+
+
+def main():
+    caves_by_name = {}
+
+    with open(sys.argv[1]) as f:
+        for line in f.readlines():
+            cave1_name, cave2_name = line.strip().split('-')
+            cave1 = caves_by_name.setdefault(cave1_name, Cave(cave1_name))
+            cave2 = caves_by_name.setdefault(cave2_name, Cave(cave2_name))
+            cave1.neighbors.append(cave2)
+            cave2.neighbors.append(cave1)
+
+    part1(caves_by_name)
+    part2(caves_by_name)
+
+
+def part1(caves_by_name):
+    start = caves_by_name['start']
+    end = caves_by_name['end']
+
+    valid_paths = []
+    queue = [State(start, {'start'}, ['start'])]
+    while queue:
+        current = queue.pop()
+        for neighbor in current.cave.neighbors:
+            if neighbor == end:
+                valid_paths.append(current.path)
+                continue
+            if neighbor.name in current.visited:
+                continue
+            new_visited = set(current.visited)
+            if neighbor.small:
+                new_visited.add(neighbor.name)
+            new_path = list(current.path)
+            new_path.append(neighbor.name)
+            queue.append(State(neighbor, new_visited, new_path))
+
+    print(len(valid_paths))
+
+
+def part2(caves_by_name):
+    start = caves_by_name['start']
+    end = caves_by_name['end']
+
+    valid_paths = set()
+
+    def bfs():
+        queue = [State(start, Counter(['start']), ['start'])]
+        while queue:
+            current = queue.pop()
+            for neighbor in current.cave.neighbors:
+                if neighbor == end:
+                    valid_paths.add(tuple(current.path))
+                    continue
+                if current.visited[neighbor.name] >= neighbor.max_visits:
+                    continue
+                new_visited = Counter(current.visited)
+                new_visited[neighbor.name] += 1
+                new_path = list(current.path)
+                new_path.append(neighbor.name)
+                queue.append(State(neighbor, new_visited, new_path))
+
+    bfs()
+
+    # for each small cave, try the BFS with 2 visits to that cave (we'll hit a lot of duplicate paths but we're deduping)
+    for cave in caves_by_name.values():
+        if cave.small and cave is not start:
+            cave.max_visits = 2
+            bfs()
+            cave.max_visits = 1
+
+    print(len(valid_paths))
+
+
+if __name__ == '__main__':
+    main()
