ac2d248c68d9b5515c3952429a02cd1561230f7f
DJ Lee <dennisjlee@gmail.com>
2021-12-27 00:43:54 -0500

    solved 2021 day 23 phew

--- 2021/day23.py
+++ 2021/day23.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 import functools
 import heapq
 import math
+import time
 from collections import deque, defaultdict
 import itertools
 import re
 import sys
 import typing
+from typing import Optional
 from copy import copy, deepcopy
 
 SIMPLE_INPUT = """
@@ -64,12 +66,12 @@ STEP_COSTS = {
 
 
 class Edge(typing.NamedTuple):
-    node_id: str
+    node_x: int
     distance: int
 
 
 class Move(typing.NamedTuple):
-    dest_id: id
+    dest_x: int
     distance: int
 
 
@@ -88,8 +90,8 @@ class Node:
         self.edges = []
 
     def connect(self, other: Node, distance: int):
-        self.edges.append(Edge(other.id, distance))
-        other.edges.append(Edge(self.id, distance))
+        self.edges.append(Edge(other.x, distance))
+        other.edges.append(Edge(self.x, distance))
 
     def push_occupant(self, occupant: str) -> int:
         self.occupants.append(occupant)
@@ -132,36 +134,36 @@ class Room(Node):
             return
 
         letter_to_move = self.occupants[-1]
-        queue = deque([(self.id, 0)])
+        queue = deque([(self.x, 0)])
         distances = defaultdict(lambda: 1000)
         while queue:
-            node_id, distance = queue.popleft()
-            node = state.nodes[node_id]
+            node_x, distance = queue.popleft()
+            node = state.nodes[node_x]
 
             # Prefer moving straight into the appropriate room before considering moves to the hallway
             if node is not self and isinstance(node, Room):
-                yield Move(node_id, distance)
+                yield Move(node_x, distance)
                 return
 
-            if distance < distances[node.id]:
-                distances[node.id] = distance
+            if distance < distances[node.x]:
+                distances[node.x] = distance
             else:
                 continue
 
             for edge in node.edges:
                 new_distance = distance + edge.distance
-                if edge.node_id not in distances and state.nodes[edge.node_id].can_accept(letter_to_move):
-                    queue.append((edge.node_id, new_distance))
+                if edge.node_x not in distances and state.nodes[edge.node_x].can_accept(letter_to_move):
+                    queue.append((edge.node_x, new_distance))
 
-        for node_id, distance in distances.items():
-            if node_id != self.id:
-                yield Move(node_id, distance)
+        for node_x, distance in distances.items():
+            if node_x != self.x:
+                yield Move(node_x, distance)
 
     def can_accept(self, letter: str):
         return letter == self.letter and not self.occupied and all(o == self.letter for o in self.occupants)
 
     def __repr__(self):
-        return f'Room(capacity={self.capacity}, letter={self.letter}, occupants=[{",".join(self.occupants)}])'
+        return f'Room(x={self.x}, capacity={self.capacity}, letter={self.letter}, occupants=[{",".join(self.occupants)}])'
 
     def clone(self):
         clone = Room(self.x, self.id, self.capacity, self.letter)
@@ -179,33 +181,26 @@ class Hallway(Node):
             return
 
         letter_to_move = self.occupants[0]
-        queue = deque([(self.id, 0)])
-        distances = defaultdict(lambda: 1000)
-        while queue:
-            node_id, distance = queue.popleft()
-            node = state.nodes[node_id]
-            # Our goal is a move into the appropriate room, so if we find a path to it, take it
-            if isinstance(node, Room):
-                yield Move(node_id, distance)
-                return
+        dest_x = X_BY_ROOM_NAME[letter_to_move]
+        if self.x < dest_x:
+            for x in range(self.x + 1, dest_x):
+                if x not in ROOM_NAMES_BY_X and state.nodes[x].occupied:
+                    return
+        else:
+            for x in range(dest_x + 1, self.x):
+                if x not in ROOM_NAMES_BY_X and state.nodes[x].occupied:
+                    return
 
-            if distance < distances[node.id]:
-                distances[node.id] = distance
-            else:
-                continue
-            if node.occupied and node != self:
-                continue
+        if not state.nodes[dest_x].can_accept(letter_to_move):
+            return
 
-            for edge in node.edges:
-                new_distance = distance + edge.distance
-                if edge.node_id not in distances and state.nodes[edge.node_id].can_accept(letter_to_move):
-                    queue.append((edge.node_id, new_distance))
+        yield Move(dest_x, abs(dest_x - self.x) + 1)
 
     def can_accept(self, letter: str):
         return not self.occupied
 
     def __repr__(self):
-        return f'Hallway(occupants=[{",".join(self.occupants)}])'
+        return f'Hallway(x={self.x}, occupants=[{",".join(self.occupants)}])'
 
     def clone(self):
         clone = Hallway(self.x, self.id)
@@ -216,46 +211,52 @@ class Hallway(Node):
 
 @functools.total_ordering
 class State:
-    nodes: dict[str, Node]
+    nodes: list[Optional[Node]]
     energy_used: int
 
-    def __init__(self, nodes: dict[str, Node], energy_used: int):
+    def __init__(self, nodes: list[Optional[Node]], energy_used: int):
         self.nodes = nodes
         self.energy_used = energy_used
 
     def is_complete(self):
-        return all(r.is_complete() for r in self.nodes.values() if isinstance(r, Room))
+        return all(r.is_complete() for r in self.nodes if r and isinstance(r, Room))
 
     def __hash__(self):
-        val = tuple(tuple(n.occupants) for _, n in sorted(self.nodes.items()))
+        val = tuple(tuple(n.occupants) for n in self.nodes if n)
         return hash((self.energy_used, val))
 
     @functools.cached_property
     def heuristic(self):
         h = 0
-        for node_id, node in self.nodes.items():
-            if isinstance(node, Hallway) and node.occupants:
+        for x, node in enumerate(self.nodes):
+            if x == 0:
+                continue
+            elif x in {3, 5, 7, 9}:
+                if node.occupants:
+                    current_height = len(node.occupants)
+                    try:
+                        non_matching_index = next(i for i, v in enumerate(node.occupants) if v != node.letter)
+                        for j in range(non_matching_index, current_height):
+                            letter = node.occupants[j]
+                            dest_x = X_BY_ROOM_NAME[letter]
+                            h += (current_height - j + 1 + abs(node.x - dest_x)) * STEP_COSTS[letter]
+                            if j < current_height - 1:
+                                # this amphipod is blocked, so give it an additional penalty
+                                h += 3 * STEP_COSTS[letter]
+                    except StopIteration:
+                        # no non-matching indexes found
+                        continue
+            elif node.occupants:
                 letter = node.occupants[0]
                 dest_x = X_BY_ROOM_NAME[letter]
                 h += (1 + abs(node.x - dest_x)) * STEP_COSTS[letter]
-            elif isinstance(node, Room) and node.occupants:
-                current_height = len(node.occupants)
-                try:
-                    non_matching_index = next(i for i, v in enumerate(node.occupants) if v != node.letter)
-                    for j in range(non_matching_index, current_height):
-                        letter = node.occupants[j]
-                        dest_x = X_BY_ROOM_NAME[letter]
-                        h += (current_height - j + 1 + abs(node.x - dest_x)) * STEP_COSTS[letter]
-                except StopIteration:
-                    # no non-matching indexes found
-                    continue
         return h
 
     def __eq__(self, other):
-        return isinstance(other, State) and hash(self) == hash(other)
+        return hash(self) == hash(other)
 
     def __ne__(self, other):
-        return not (isinstance(other, State) and hash(self) == hash(other))
+        return hash(self) != hash(other)
 
     def __lt__(self, other):
         return (self.energy_used + self.heuristic) < (other.energy_used + other.heuristic)
@@ -264,13 +265,15 @@ class State:
         return f'State(energy_used={self.energy_used}, heuristic={self.heuristic})'
 
     def successor_states(self) -> typing.Iterable[State]:
-        for n_id, node in self.nodes.items():
+        for n_x, node in enumerate(self.nodes):
+            if n_x == 0:
+                continue
             for move in node.possible_moves(self):
                 new_nodes = copy(self.nodes)
                 src = node.clone()
-                new_nodes[n_id] = src
-                dest = new_nodes[move.dest_id].clone()
-                new_nodes[move.dest_id] = dest
+                new_nodes[n_x] = src
+                dest = new_nodes[move.dest_x].clone()
+                new_nodes[move.dest_x] = dest
 
                 letter, src_internal_distance = src.pop_occupant()
                 dest_internal_distance = dest.push_occupant(letter)
@@ -289,7 +292,9 @@ class Amphipod:
 
 
 def main():
+    start = time.perf_counter()
     # part 1 solved w/ pen and paper lol - answer was 11536
+
     # print(find_best_solution1(SIMPLE_INPUT))
     # print(find_best_solution2(SIMPLE_INPUT))
 
@@ -297,7 +302,9 @@ def main():
     # print(find_best_solution1(EXAMPLE_INPUT))
 
     # part 2
-    # print(find_best_solution1(PART2_INPUT))
+    print(find_best_solution1(PART2_INPUT))
+    end = time.perf_counter()
+    print(f'time: {end - start}s')
 
 
 def find_best_solution1(raw_input: str):
@@ -331,17 +338,18 @@ def preferential_dfs(state):
 
 def a_star_search(initial_state):
     heap = [initial_state]
-    state_results = {}
+    # state_results = {}
     counter = 0
     while heap:
         counter += 1
         state = heapq.heappop(heap)
-        if state in state_results and state.energy_used > state_results[state]:
-            continue
-        state_results[state] = state.energy_used
+        # if state in state_results and state.energy_used > state_results[state]:
+        #     continue
+        # state_results[state] = state.energy_used
         if counter % 1000 == 0:
             print(f'counter {counter}; queue length {len(heap)}; current state: {state}')
         if state.is_complete():
+            print(f'found solution! counter {counter}; queue length {len(heap)}; current state: {state}')
             return state.energy_used
 
         num_successors = 0
@@ -361,40 +369,40 @@ def a_star_search(initial_state):
 def parse_nodes(lines: list[str]):
     room_capacity = len(lines) - 3
 
-    nodes: dict[str, Node] = {}
+    nodes: list[Optional[Node]] = [None] * 12
     for col_index, letter in ROOM_NAMES_BY_X.items():
-        nodes[letter] = Room(col_index, letter, room_capacity, letter)
-        fill_room(lines, col_index, nodes[letter])
+        nodes[col_index] = Room(col_index, letter, room_capacity, letter)
+        fill_room(lines, col_index, nodes[col_index])
 
     left1 = Hallway(1, 'L1')
     left2 = Hallway(2, 'L2')
     left1.connect(left2, 1)
 
-    left2.connect(nodes['A'], 2)
+    left2.connect(nodes[3], 2)
     ab = Hallway(4, 'AB')
     left2.connect(ab, 2)
-    ab.connect(nodes['A'], 2)
-    ab.connect(nodes['B'], 2)
+    ab.connect(nodes[3], 2)
+    ab.connect(nodes[5], 2)
 
     bc = Hallway(6, 'BC')
     bc.connect(ab, 2)
-    bc.connect(nodes['B'], 2)
-    bc.connect(nodes['C'], 2)
+    bc.connect(nodes[5], 2)
+    bc.connect(nodes[7], 2)
 
     cd = Hallway(8, 'CD')
     cd.connect(bc, 2)
-    cd.connect(nodes['C'], 2)
-    cd.connect(nodes['D'], 2)
+    cd.connect(nodes[7], 2)
+    cd.connect(nodes[9], 2)
 
     right2 = Hallway(10, 'R2')
     right2.connect(cd, 2)
-    right2.connect(nodes['D'], 2)
+    right2.connect(nodes[9], 2)
 
     right1 = Hallway(11, 'R1')
     right1.connect(right2, 1)
 
     for h in (left1, left2, ab, bc, cd, right2, right1):
-        nodes[h.id] = h
+        nodes[h.x] = h
     return State(nodes, 0)
 
 
