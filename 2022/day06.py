b537c918b49033913722ea3909cd0c37f439de03
DJ Lee <dennisjlee@gmail.com>
2022-12-06 00:36:49 -0500

    2022 day 6, try a few different methods

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,22 +1,59 @@
 from __future__ import annotations
+from collections import Counter, deque
 import sys
-
+import timeit
 
 def main():
     with open(sys.argv[1]) as f:
         buffer = f.read().strip()
 
-    for i in range(3, len(buffer)):
-        test = set(buffer[i-3:i+1])
-        if len(test) == 4:
-            print(i + 1)
-            break
+    print('easy way', easy_way(buffer, 4), easy_way(buffer, 14))
+    print('easy way time', timeit.timeit(lambda: easy_way(buffer, 14), number=1000))
+    print('hard way', hard_way(buffer, 4), hard_way(buffer, 14))
+    print('hard way time', timeit.timeit(lambda: hard_way(buffer, 14), number=1000))
+    print('deque way', deque_way(buffer, 4), deque_way(buffer, 14))
+    print('deque way time', timeit.timeit(lambda: deque_way(buffer, 14), number=1000))
+    print('dedupe way', dedupe_way(buffer, 4), dedupe_way(buffer, 14))
+    print('dedupe way time', timeit.timeit(lambda: dedupe_way(buffer, 14), number=1000))
 
-    for i in range(13, len(buffer)):
-        test = set(buffer[i-13:i+1])
-        if len(test) == 14:
-            print(i + 1)
-            break
+
+def easy_way(buffer, n):
+    for i in range(n, len(buffer)):
+        test = set(buffer[i-n:i])
+        if len(test) == n:
+            return i
+
+
+def hard_way(buffer, n):
+    counter = Counter(buffer[:n])
+    for i in range(n, len(buffer)):
+        if len(counter) == n:
+            return i
+        left = buffer[i-n]
+        counter[left] -= 1
+        if counter[left] == 0:
+            del counter[left]
+        counter[buffer[i]] += 1
+
+
+def deque_way(buffer, n):
+    dq = deque(buffer[:n])
+    for i in range(n, len(buffer)):
+        if len(set(dq)) == n:
+            return i
+        dq.popleft()
+        dq.append(buffer[i])
+
+
+def dedupe_way(buffer, n):
+    for i in range(n, len(buffer)):
+        s = {buffer[i-n]}
+        for j in range(i-n+1, i):
+            if buffer[j] in s:
+                break
+            s.add(buffer[j])
+        else:
+            return i
 
 
 if __name__ == '__main__':
