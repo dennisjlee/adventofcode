bf825f985f32fbf9b3ece100d936b15fb8c1ff01
DJ Lee <dennisjlee@gmail.com>
2022-12-17 00:00:00 -0500

    wip 2022 day 16 different approach

--- aoc2022/day16.py
+++ aoc2022/day16.py
@@ -9,7 +9,7 @@ from copy import deepcopy
 class Node(NamedTuple):
     name: str
     flow_rate: int
-    neighbors: list[Node]
+    edges: dict[str, int]
 
 
 PARSE_PATTERN = re.compile(r'Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? (.*?)$', re.MULTILINE)
@@ -50,6 +50,12 @@ class State(NamedTuple):
         return sum(self.nodes_by_name[name].flow_rate for name in self.open_valve_names)
 
 
+def find_paths_to_relevant_nodes(nodes_by_name: dict[str, Node], node: Node) -> dict[str, int]:
+    # run a version of dijkstra on the base graph to construct edges for the meta-graph
+    # TODO
+    return {}
+
+
 def main():
     nodes_by_name = {}
     with open(sys.argv[1]) as f:
@@ -57,35 +63,39 @@ def main():
         for match in PARSE_PATTERN.finditer(contents):
             name = match.group(1)
             flow_rate = int(match.group(2))
-            node = Node(name, flow_rate, [])
+            node = Node(name, flow_rate, {})
             nodes_by_name[name] = node
 
         for match in PARSE_PATTERN.finditer(contents):
             name = match.group(1)
             neighbor_names = match.group(3).split(', ')
             node = nodes_by_name[name]
-            node.neighbors.extend(nodes_by_name[nn] for nn in neighbor_names)
+            node.edges.update({nn: 1 for nn in neighbor_names})
 
-    print(nodes_by_name)
-    states = deque([State(0, 0, 'AA', frozenset(), nodes_by_name)])
-    best_pressure_released = 0
-    step = 0
-    while states:
-        step += 1
-        state = states.popleft()
-        if step % 1000 == 0:
-            print(f'step {step}, # states {len(states)}, current state time {state.time}')
-        if state.time >= 29:
-            best_pressure_released = max(best_pressure_released, state.next_pressure_released())
-            continue
-        if state.can_open():
-            states.append(state.iterate_open_valve())
-        for neighbor in state.neighbors():
-            states.append(state.iterate_move_to(neighbor))
-
-    print(best_pressure_released)
+    # Construct meta-graph where only AA and nodes with non-zero flow rate are represented,
+    # and every node is connected to every other with weighted edges. Each edge weight between
+    # nodes N1 and N2 is defined as the shortest path between N1 and N2 in the base graph.
 
 
+
+    # print(nodes_by_name)
+    # states = deque([State(0, 0, 'AA', frozenset(), nodes_by_name)])
+    # best_pressure_released = 0
+    # step = 0
+    # while states:
+    #     step += 1
+    #     state = states.popleft()
+    #     if step % 1000 == 0:
+    #         print(f'step {step}, # states {len(states)}, current state time {state.time}')
+    #     if state.time >= 29:
+    #         best_pressure_released = max(best_pressure_released, state.next_pressure_released())
+    #         continue
+    #     if state.can_open():
+    #         states.append(state.iterate_open_valve())
+    #     for neighbor in state.neighbors():
+    #         states.append(state.iterate_move_to(neighbor))
+    #
+    # print(best_pressure_released)
 
 
 if __name__ == '__main__':
