d6789bebe822e5ec3821eb48d2eff1220c818ba5
DJ Lee <dennisjlee@gmail.com>
2022-12-30 00:23:59 -0500

    2022 day 22 part 2

--- aoc2022/day22.py
+++ aoc2022/day22.py
@@ -2,14 +2,18 @@ from __future__ import annotations
 
 import re
 import sys
-from operator import add, sub, mul, floordiv
 from typing import Optional, NamedTuple, Literal
 
 MOVE_REGEX = re.compile(r'(\d+)([LR]?)')
 
-FACINGS = ['r', 'd', 'l', 'u']
-FACING_MAP = {f: i for f, i in enumerate(FACINGS)}
 
+class Facings:
+    RIGHT = 0
+    DOWN = 1
+    LEFT = 2
+    UP = 3
+
+FACING_NAMES = ['right', 'down', 'left', 'up']
 
 Rotation = Literal['L', 'R']
 
@@ -20,6 +24,7 @@ class Point(NamedTuple):
 
 
 DELTAS = [Point(1, 0), Point(0, 1), Point(-1, 0), Point(0, -1)]
+
 
 class Interval(NamedTuple):
     start: int
@@ -34,14 +39,33 @@ class Move(NamedTuple):
         return f'Move({self.rotation or self.magnitude})'
 
 
+class Node(NamedTuple):
+    value: str
+    point: Point
+    neighbors: list[Optional[Node]]
+    facing_changes: dict[int, int]
+
+    def __repr__(self):
+        return f'Node(\'{self.value}\', {self.point})'
+
+
 def main():
     with open(sys.argv[1]) as f:
         board, path = f.read().split('\n\n')
 
-    part1(board, path)
+
+    moves = []
+    for match in MOVE_REGEX.finditer(path):
+        magnitude = int(match.group(1))
+        moves.append(Move(magnitude, None))
+        if match.group(2):
+            moves.append(Move(None, match.group(2)))
+
+    part1(board, moves)
+    part2(board, moves)
 
 
-def part1(board: str, path: str):
+def part1(board: str, moves: list[Move]):
     grid: list[list[Optional[str]]] = []
     x_bounds: list[Interval] = []
     y_bounds: list[Interval] = []
@@ -58,13 +82,6 @@ def part1(board: str, path: str):
         populated_y_indices = [y for y in range(height) if len(grid[y]) > x and grid[y][x] is not None]
         y_bounds.append(Interval(min(populated_y_indices), max(populated_y_indices)))
 
-    moves = []
-    for match in MOVE_REGEX.finditer(path):
-        magnitude = int(match.group(1))
-        moves.append(Move(magnitude, None))
-        if match.group(2):
-            moves.append(Move(None, match.group(2)))
-
     cy = 0
     cx = x_bounds[0].start
     facing = 0
@@ -76,7 +93,7 @@ def part1(board: str, path: str):
             elif move.rotation == 'L':
                 facing = (facing - 1) % 4
         else:
-            if facing == 0:
+            if facing == Facings.RIGHT:
                 bounds = x_bounds[cy]
                 for _ in range(move.magnitude):
                     if cx == bounds.end:
@@ -87,7 +104,7 @@ def part1(board: str, path: str):
                         break
                     else:
                         cx = nx
-            elif facing == 1:
+            elif facing == Facings.DOWN:
                 bounds = y_bounds[cx]
                 for _ in range(move.magnitude):
                     if cy == bounds.end:
@@ -98,7 +115,7 @@ def part1(board: str, path: str):
                         break
                     else:
                         cy = ny
-            elif facing == 2:
+            elif facing == Facings.LEFT:
                 bounds = x_bounds[cy]
                 for _ in range(move.magnitude):
                     if cx == bounds.start:
@@ -109,7 +126,7 @@ def part1(board: str, path: str):
                         break
                     else:
                         cx = nx
-            elif facing == 3:
+            elif facing == Facings.UP:
                 bounds = y_bounds[cx]
                 for _ in range(move.magnitude):
                     if cy == bounds.start:
@@ -123,6 +140,170 @@ def part1(board: str, path: str):
 
     print(cy + 1, cx + 1, facing)
     print(1000 * (cy + 1) + 4 * (cx + 1) + facing)
+
+
+def part2(board: str, moves: list[Move]):
+    grid: list[list[Optional[Node]]] = []
+
+    for y, line in enumerate(board.split('\n')):
+        row = [Node(c, Point(x, y), neighbors=[None]*4, facing_changes={})
+               if c != ' ' else None for x, c in enumerate(line)]
+        grid.append(row)
+
+    for y in range(50):
+        # square 1
+        for x in range(50, 100):
+            node = grid[y][x]
+            node.neighbors[Facings.RIGHT] = grid[y][x + 1]
+            node.neighbors[Facings.DOWN] = grid[y + 1][x]
+            if y > 0:
+                node.neighbors[Facings.UP] = grid[y - 1][x]
+            else:
+                # link to left edge of square 6
+                node.neighbors[Facings.UP] = grid[100 + x][0]
+                node.facing_changes[Facings.UP] = Facings.RIGHT
+
+            if x > 50:
+                node.neighbors[Facings.LEFT] = grid[y][x - 1]
+            else:
+                # link to left edge of square 4
+                node.neighbors[Facings.LEFT] = grid[149 - y][0]
+                node.facing_changes[Facings.LEFT] = Facings.RIGHT
+
+        # square 2
+        for x in range(100, 150):
+            node = grid[y][x]
+            node.neighbors[Facings.LEFT] = grid[y][x - 1]
+            if y > 0:
+                node.neighbors[Facings.UP] = grid[y - 1][x]
+            else:
+                # link to bottom edge of square 6, still facing up
+                node.neighbors[Facings.UP] = grid[199][x - 100]
+
+            if y < 49:
+                node.neighbors[Facings.DOWN] = grid[y + 1][x]
+            else:
+                # link to right edge of square 3
+                node.neighbors[Facings.DOWN] = grid[x - 50][99]
+                node.facing_changes[Facings.DOWN] = Facings.LEFT
+
+            if x < 149:
+                node.neighbors[Facings.RIGHT] = grid[y][x + 1]
+            else:
+                # link to right edge of square 5
+                node.neighbors[Facings.RIGHT] = grid[149 - y][99]
+                node.facing_changes[Facings.RIGHT] = Facings.LEFT
+
+    for y in range(50, 100):
+        # square 3
+        for x in range(50, 100):
+            node = grid[y][x]
+            node.neighbors[Facings.UP] = grid[y - 1][x]
+            node.neighbors[Facings.DOWN] = grid[y + 1][x]
+            if x > 50:
+                node.neighbors[Facings.LEFT] = grid[y][x - 1]
+            else:
+                # link to top edge of square 4
+                node.neighbors[Facings.LEFT] = grid[100][y - 50]
+                node.facing_changes[Facings.LEFT] = Facings.DOWN
+
+            if x < 99:
+                node.neighbors[Facings.RIGHT] = grid[y][x + 1]
+            else:
+                # link to bottom edge of square 2
+                node.neighbors[Facings.RIGHT] = grid[49][y + 50]
+                node.facing_changes[Facings.RIGHT] = Facings.UP
+
+    for y in range(100, 150):
+        # square 4
+        for x in range(50):
+            node = grid[y][x]
+            node.neighbors[Facings.RIGHT] = grid[y][x + 1]
+            node.neighbors[Facings.DOWN] = grid[y + 1][x]
+            if y > 100:
+                node.neighbors[Facings.UP] = grid[y - 1][x]
+            else:
+                # link to left edge of square 3
+                node.neighbors[Facings.UP] = grid[x + 50][50]
+                node.facing_changes[Facings.UP] = Facings.RIGHT
+
+            if x > 0:
+                node.neighbors[Facings.LEFT] = grid[y][x - 1]
+            else:
+                # link to left edge of square 1
+                node.neighbors[Facings.LEFT] = grid[149 - y][50]
+                node.facing_changes[Facings.LEFT] = Facings.RIGHT
+
+        # square 5
+        for x in range(50, 100):
+            node = grid[y][x]
+            node.neighbors[Facings.LEFT] = grid[y][x - 1]
+            node.neighbors[Facings.UP] = grid[y - 1][x]
+
+            if x < 99:
+                node.neighbors[Facings.RIGHT] = grid[y][x + 1]
+            else:
+                # link to right edge of square 2
+                node.neighbors[Facings.RIGHT] = grid[149 - y][149]
+                node.facing_changes[Facings.RIGHT] = Facings.LEFT
+
+            if y < 149:
+                node.neighbors[Facings.DOWN] = grid[y + 1][x]
+            else:
+                # link to right edge of square 6
+                node.neighbors[Facings.DOWN] = grid[x + 100][49]
+                node.facing_changes[Facings.DOWN] = Facings.LEFT
+
+    for y in range(150, 200):
+        # square 6
+        for x in range(50):
+            node = grid[y][x]
+            node.neighbors[Facings.UP] = grid[y - 1][x]
+
+            if y < 199:
+                node.neighbors[Facings.DOWN] = grid[y + 1][x]
+            else:
+                # link to top edge of square 2, still facing down
+                node.neighbors[Facings.DOWN] = grid[0][x + 100]
+
+            if x > 0:
+                node.neighbors[Facings.LEFT] = grid[y][x - 1]
+            else:
+                # link to top edge of square 1
+                node.neighbors[Facings.LEFT] = grid[0][y - 100]
+                node.facing_changes[Facings.LEFT] = Facings.DOWN
+
+            if x < 49:
+                node.neighbors[Facings.RIGHT] = grid[y][x + 1]
+            else:
+                # link to bottom edge of square 5
+                node.neighbors[Facings.RIGHT] = grid[149][y - 100]
+                node.facing_changes[Facings.RIGHT] = Facings.UP
+
+    for row in grid:
+        for node in row:
+            if node is not None:
+                assert all(node.neighbors)
+
+    curr_node = grid[0][50]
+    facing = 0
+
+    for move in moves:
+        if move.rotation:
+            if move.rotation == 'R':
+                facing = (facing + 1) % 4
+            elif move.rotation == 'L':
+                facing = (facing - 1) % 4
+        else:
+            for _ in range(move.magnitude):
+                next_node = curr_node.neighbors[facing]
+                if next_node.value == '#':
+                    break
+                facing = curr_node.facing_changes.get(facing, facing)
+                curr_node = next_node
+
+    print(curr_node.point.y + 1, curr_node.point.x + 1, facing)
+    print(1000 * (curr_node.point.y + 1) + 4 * (curr_node.point.x + 1) + facing)
 
 
 if __name__ == '__main__':
