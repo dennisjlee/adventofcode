bdad0853dd84ad0dd8f2907986e404dfcbf60021
DJ Lee <dj@breakwaterresearch.com>
2024-12-13 09:25:43 -0500

    2024 day 13 part 2

--- aoc2024/day13.py
+++ aoc2024/day13.py
@@ -1,9 +1,14 @@
+import math
+
+import numpy as np
 import re
 import sys
 from typing import NamedTuple
 
 
-CONFIG_REGEX = re.compile(r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)")
+CONFIG_REGEX = re.compile(
+    r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)"
+)
 
 
 class ClawMachine(NamedTuple):
@@ -14,7 +19,7 @@ class ClawMachine(NamedTuple):
     prize_x: int
     prize_y: int
 
-    def solve1(self) -> int | None:
+    def solve(self) -> int | None:
         x = 0
         y = 0
         i = 1
@@ -30,28 +35,50 @@ class ClawMachine(NamedTuple):
             i += 1
         return None
 
+    def solve_fast(self) -> int | None:
+        coeffs = np.array([[self.a_dx, self.b_dx], [self.a_dy, self.b_dy]])
+        constants = np.array([self.prize_x, self.prize_y])
+        [a, b] = np.linalg.solve(coeffs, constants)
+        if math.isclose(round(a), a, rel_tol=0, abs_tol=0.001) and math.isclose(
+            round(b), b, rel_tol=0, abs_tol=0.001
+        ):
+            return 3 * int(round(a)) + int(round(b))
+        else:
+            return None
+
 
 def main():
     machines: list[ClawMachine] = []
     with open(sys.argv[1]) as f:
         raw = f.read()
         for match in CONFIG_REGEX.finditer(raw):
-            machines.append(ClawMachine(
-                int(match.group(1)),
-                int(match.group(2)),
-                int(match.group(3)),
-                int(match.group(4)),
-                int(match.group(5)),
-                int(match.group(6)),
-            ))
+            machines.append(
+                ClawMachine(
+                    int(match.group(1)),
+                    int(match.group(2)),
+                    int(match.group(3)),
+                    int(match.group(4)),
+                    int(match.group(5)),
+                    int(match.group(6)),
+                )
+            )
 
     tokens_spent = 0
-    for machine in machines:
-        if (tokens := machine.solve()) is not None:
+    for m in machines:
+        if (tokens := m.solve()) is not None:
+            tokens_spent += tokens
+    print(tokens_spent)
+
+    tokens_spent = 0
+    delta = 10_000_000_000_000
+    for i, m in enumerate(machines):
+        part2_machine = ClawMachine(
+            m.a_dx, m.a_dy, m.b_dx, m.b_dy, m.prize_x + delta, m.prize_y + delta
+        )
+        if (tokens := part2_machine.solve_fast()) is not None:
             tokens_spent += tokens
     print(tokens_spent)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
