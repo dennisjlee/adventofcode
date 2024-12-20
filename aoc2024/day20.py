6d17f3aa75118224b33df08876f8c16a96f94f90
DJ Lee <dj@breakwaterresearch.com>
2024-12-21 06:25:42 +0900

    Finish 2024 day 20

--- aoc2024/day20.py
+++ aoc2024/day20.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
 import sys
-from collections import deque
+from collections import deque, defaultdict, Counter
 from typing import NamedTuple
 
 
@@ -15,6 +16,30 @@ class Point(NamedTuple):
 
     def in_range(self, w: int, h: int):
         return 0 <= self.x < w and 0 <= self.y < h
+
+    def manhattan_distance(self, other: Point):
+        return abs(other.x - self.x) + abs(other.y - self.y)
+
+
+class PathPoint(NamedTuple):
+    index: int
+    point: Point
+
+    def cheat_savings1(self, other: PathPoint) -> int:
+        cheat_dist = self.point.manhattan_distance(other.point)
+        if cheat_dist <= 2:
+            normal_dist = abs(other.index - self.index)
+            if cheat_dist < normal_dist:
+                return normal_dist - cheat_dist
+        return 0
+
+    def cheat_savings2(self, other: PathPoint) -> int:
+        cheat_dist = self.point.manhattan_distance(other.point)
+        if cheat_dist <= 20:
+            normal_dist = abs(other.index - self.index)
+            if cheat_dist < normal_dist:
+                return normal_dist - cheat_dist
+        return 0
 
 
 def main():
@@ -36,11 +61,36 @@ def main():
     assert start and end
 
     path = get_basic_path(grid, w, h, start, end)
-    print(len(path), path)
+    path_points = [PathPoint(i, point) for i, point in enumerate(path)]
 
-    # TODO: do an O(N^2) loop to see whether point i is <2 manhattan distance from point i+3..end
-    # any of those are potential shortcuts, and then we can just subtract the indices to see how much we could save
+    path_points_by_x: dict[int, list[PathPoint]] = defaultdict(list)
+    for pp in path_points:
+        path_points_by_x[pp.point.x].append(pp)
 
+    potential_good_cheats = 0
+    cheat_counter = Counter()
+    for pp in path_points:
+        for potential_x in range(pp.point.x - 2, pp.point.x + 3):
+            for other in path_points_by_x[potential_x]:
+                if other.index > pp.index:
+                    savings = pp.cheat_savings1(other)
+                    if savings:
+                        cheat_counter[savings] += 1
+                    if savings >= 100:
+                        potential_good_cheats += 1
+
+    print(potential_good_cheats)
+
+    potential_good_cheats = 0
+    for pp in path_points:
+        for potential_x in range(pp.point.x - 20, pp.point.x + 21):
+            for other in path_points_by_x[potential_x]:
+                if other.index > pp.index:
+                    savings = pp.cheat_savings2(other)
+                    if savings >= 100:
+                        potential_good_cheats += 1
+
+    print(potential_good_cheats)
 
 
 def get_basic_path(grid: list[list[str]], w: int, h: int, start: Point, end: Point):
@@ -49,7 +99,7 @@ def get_basic_path(grid: list[list[str]], w: int, h: int, start: Point, end: Poi
         path, visited = q.pop()
         for n in path[-1].neighbors():
             if n == end:
-                return path
+                return path + [end]
             if n.in_range(w, h) and grid[n.y][n.x] == "." and n not in path:
                 q.append((path + [n], visited | {n}))
 
