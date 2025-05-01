6d0e83c6866d285e11a267e6a3a5a0057ba6fce3
DJ Lee <dennisjlee@gmail.com>
2025-05-01 13:57:57 -0400

    2023 day 20 part 2

--- aoc2023/day20.py
+++ aoc2023/day20.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import math
 import operator
 import re
 import sys
+from argparse import ArgumentParser
 from collections import defaultdict, deque
 from enum import Enum
 from typing import NamedTuple, Literal, Iterable
@@ -33,9 +35,12 @@ class Module:
     def __repr__(self):
         return self.label
 
+    def __str__(self):
+        return self.label
+
 
 class BroadcastModule(Module):
-    def receive_pulse(self, input_label: str, pulse: Pulse) -> Iterable[Pulse]:
+    def receive_pulse(self, input_label: str, pulse: Pulse) -> Iterable[Message]:
         for output in self.output_labels:
             yield Message(pulse, self.label, output)
 
@@ -56,17 +61,20 @@ class FlipFlopModule(Module):
     def __repr__(self):
         return f'%{self.label}: {1 if self.enabled else 0}'
 
+    def __str__(self):
+        return f'%{self.label}'
+
 
 class ConjunctionModule(Module):
     def __init__(self, label: str, output_labels: list[str]):
         super().__init__(label, output_labels)
 
-        self.last_inputs = {}
+        self.last_inputs: dict[str, Pulse] = {}
 
     def set_input_labels(self, input_labels: list[str]):
         self.last_inputs = {label: Pulse.LOW for label in input_labels}
 
-    def receive_pulse(self, input_label: str, pulse: Pulse) -> Iterable[Pulse]:
+    def receive_pulse(self, input_label: str, pulse: Pulse) -> Iterable[Message]:
         self.last_inputs[input_label] = pulse
         if all(p == Pulse.HIGH for p in self.last_inputs.values()):
             for output in self.output_labels:
@@ -77,6 +85,9 @@ class ConjunctionModule(Module):
 
     def __repr__(self):
         return f'&{self.label}: {", ".join(f"{k}: {1 if v == Pulse.HIGH else 0}" for k, v in self.last_inputs.items())}'
+
+    def __str__(self):
+        return f'&{self.label}'
 
 
 PARSE_REGEX = re.compile(r'([&%]?)(\w+) -> (.*)$')
@@ -135,11 +146,20 @@ def parse_modules(lines: list[str], verbose=False) -> dict[str, Module]:
 
 
 def main():
-    with open(sys.argv[1]) as f:
+    parser = ArgumentParser()
+    parser.add_argument('--graphviz', action='store_true')
+    parser.add_argument('input', help='input file')
+
+    args = parser.parse_args()
+
+    with open(args.input) as f:
         lines = f.readlines()
 
-    print(part1(lines))
-    print(part2(lines))
+    if args.graphviz:
+        print_graphviz(lines)
+    else:
+        print(part1(lines))
+        print(part2(lines))
 
 
 def part1(lines: list[str]):
@@ -160,18 +180,82 @@ def part1(lines: list[str]):
 
 
 def part2(lines: list[str]):
-    keyed_modules = parse_modules(lines, True)
+    keyed_modules = parse_modules(lines)
 
     queue = deque()
-    for i in range(1_000):
+    first_high_pulse: dict[str, int] = {}
+    for i in range(1, 10_000):
         queue.append(Message(Pulse.LOW, 'button', 'broadcaster'))
         while queue:
             pulse, source, target = queue.popleft()
-            # if target == 'rx':
-            #     print('\n', i, pulse)
+
+            # `kj` is the conjunction module that is the only input to `rx`,
+            # so it needs to receive 4 HIGH pulses from each of its inputs.
+            # Based on observation, each of those inputs will produce a HIGH
+            # on a recurring cycle, which happens to be a prime around 4000.
+            if target == 'kj' and pulse == Pulse.HIGH:
+                if source not in first_high_pulse:
+                    first_high_pulse[source] = i
             module = keyed_modules[target]
             queue.extend(module.receive_pulse(source, pulse))
+        if len(first_high_pulse) == 4:
+            return math.lcm(*first_high_pulse.values())
     return None
+
+
+def print_graphviz(lines: list[str]):
+    # pipe this output to `dot -Tsvg > aoc2023/inputs/day20.svg`)
+
+    keyed_modules = parse_modules(lines)
+    ranks_by_module = {}
+    queue = deque([('broadcaster', 0)])
+    while queue:
+        label, rank = queue.popleft()
+        if label in ranks_by_module:
+            continue
+        ranks_by_module[label] = rank
+        for output_label in keyed_modules[label].output_labels:
+            queue.append((output_label, rank + 1))
+
+    modules_by_rank = defaultdict(list)
+    for label, rank in ranks_by_module.items():
+        modules_by_rank[rank].append(label)
+
+    print("strict digraph {")
+    print("rankdir=LR;")
+    print("node[ordering = out];")
+
+    for module in keyed_modules.values():
+        print(f'{module.label} [label="{str(module)}"];')
+        for output_label in module.output_labels:
+            print(f"{module.label} -> {output_label};")
+
+    print(f"""
+    {{
+        rank = source;
+        broadcaster;
+    }}
+    """)
+    print(f"""
+    {{
+        rank = sink;
+        rx;
+    }}
+    """)
+    #
+    # for rank, labels in sorted(modules_by_rank.items()):
+    #     # https://stackoverflow.com/a/64007295
+    #     print(f"""
+    #     {{
+    #         rank = same;
+    #         rankdir = TB;
+    #         // Here we enforce the desired order with "invisible" edges and arrowheads
+    #         edge [style=invis];
+    #         {' -> '.join(labels)};
+    #     }}
+    #     """)
+
+    print("}")
 
 
 if __name__ == '__main__':
