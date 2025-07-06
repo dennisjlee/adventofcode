0368fb33bc2b0623fbe3e28e9de24225818760f2
DJ Lee <dennisjlee@gmail.com>
2025-07-06 10:17:46 -0400

    2019 day 12 part 2

--- aoc2019/src/day12.rs
+++ aoc2019/src/day12.rs
@@ -1,4 +1,6 @@
+use num::integer::lcm;
 use regex::Regex;
+use std::collections::HashMap;
 use std::io::Read;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -73,7 +75,7 @@ pub fn run(input_filename: &str) -> std::io::Result<()> {
     file.read_to_string(&mut contents)?;
 
     let re = Regex::new(r"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>").unwrap();
-
+    
     let mut moons = Vec::new();
     for (_, [x, y, z]) in re.captures_iter(&contents).map(|c| c.extract()) {
         moons.push(Moon {
@@ -90,7 +92,10 @@ pub fn run(input_filename: &str) -> std::io::Result<()> {
         });
     }
 
-    for _step in 0..1000 {
+    let mut previous_x_states: HashMap<Vec<i32>, i32> = HashMap::new();
+    let mut previous_y_states: HashMap<Vec<i32>, i32> = HashMap::new();
+    let mut previous_z_states: HashMap<Vec<i32>, i32> = HashMap::new();
+    for step in 0..300_000 {
         for i in 0..moons.len() - 1 {
             let (split1, split2) = moons.split_at_mut(i + 1);
             for j in 0..split2.len() {
@@ -100,8 +105,43 @@ pub fn run(input_filename: &str) -> std::io::Result<()> {
         for moon in &mut moons {
             moon.apply_velocity();
         }
+
+        if step == 999 {
+            // Part 1: Calculate total energy after 1000 steps
+            println!("{}", moons.iter().map(Moon::total_energy).sum::<i32>());
+        }
+
+        let x_state: Vec<i32> = moons
+            .iter()
+            .flat_map(|m| vec![m.position.x, m.velocity.dx])
+            .collect();
+        let y_state: Vec<i32> = moons
+            .iter()
+            .flat_map(|m| vec![m.position.y, m.velocity.dy])
+            .collect();
+        let z_state: Vec<i32> = moons
+            .iter()
+            .flat_map(|m| vec![m.position.z, m.velocity.dz])
+            .collect();
+        let previous_x_step = previous_x_states.insert(x_state, step);
+        let previous_y_step = previous_y_states.insert(y_state, step);
+        let previous_z_step = previous_z_states.insert(z_state, step);
+        if previous_x_step.is_some() && previous_y_step.is_some() && previous_z_step.is_some() {
+            println!(
+                "Part 2: Found repeat in all axes at step {step}, previously at x={}, y={}, z={}",
+                previous_x_step.unwrap(),
+                previous_y_step.unwrap(),
+                previous_z_step.unwrap()
+            );
+
+            // Calculate the least common multiple of the steps for each axis
+            let x_cycle_size = (step - previous_x_step.unwrap()) as i64;
+            let y_cycle_size = (step - previous_y_step.unwrap()) as i64;
+            let z_cycle_size = (step - previous_z_step.unwrap()) as i64;
+            println!("{}", lcm(lcm(x_cycle_size, y_cycle_size), z_cycle_size));
+            break;
+        }
     }
-    println!("{}", moons.iter().map(Moon::total_energy).sum::<i32>());
 
     Ok(())
 }
