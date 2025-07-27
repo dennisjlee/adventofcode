435fa65778b35032ba6234d8dc629b80d493720e
DJ Lee <dennisjlee@gmail.com>
2025-07-27 16:32:54 -0400

    WIP to use CrossTerm to build an interactive terminal game for day 13

--- aoc2019/src/day13.rs
+++ aoc2019/src/day13.rs
@@ -1,13 +1,18 @@
 use crate::intcode::{IOModule, IntCode};
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor,
+    event::{Event, KeyCode, poll, read},
+    style, terminal,
+};
 use std::collections::HashMap;
-use std::io::Read;
+use std::io::{self, Read, Stdout, Write};
+use std::time;
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug, Copy)]
 struct Point {
     x: i32,
     y: i32,
 }
-
 
 #[derive(PartialEq, Eq, Clone, Debug, Copy)]
 enum JoystickState {
@@ -16,30 +21,51 @@ enum JoystickState {
     Right = 1,
 }
 
-
-struct ArcadeIOModule {
+struct ArcadeIOModule<'a> {
     output_buffer: [i64; 3],
     output_index: usize,
     score: u32,
+    score_history: Vec<(u32, usize)>,
     joystick_state: JoystickState,
     screen: HashMap<Point, u8>,
     verbose: bool,
+    first_input_requested: bool,
+    stdout: Option<&'a mut Stdout>,
 }
 
-impl ArcadeIOModule {
-    pub fn new(verbose: bool) -> Self {
-        ArcadeIOModule {
+impl<'a> ArcadeIOModule<'a> {
+    pub fn new(verbose: bool, stdout: Option<&'a mut Stdout>) -> Self {
+        let mut arcade_module = ArcadeIOModule {
             output_buffer: [0; 3],
             output_index: 0,
             score: 0,
+            score_history: Vec::new(),
             joystick_state: JoystickState::Neutral,
             screen: HashMap::new(),
-            verbose
-        }
+            first_input_requested: false,
+            verbose,
+            stdout,
+        };
+
+        arcade_module.initialize().unwrap();
+        arcade_module
     }
 
     pub fn count_tiles(&self, tile_id: u8) -> usize {
         self.screen.values().filter(|&&id| id == tile_id).count()
+    }
+
+    pub fn score_history(&self) -> &[(u32, usize)] {
+        &self.score_history
+    }
+
+    fn initialize(&mut self) -> io::Result<()> {
+        if let Some(output) = self.stdout.as_mut() {
+            terminal::enable_raw_mode()?;
+            output.execute(terminal::Clear(terminal::ClearType::All))?;
+            output.execute(cursor::Hide)?;
+        }
+        Ok(())
     }
 
     pub fn render_screen(&self) {
@@ -66,22 +92,27 @@ impl ArcadeIOModule {
         for y in min_y..=max_y {
             for x in min_x..=max_x {
                 let tile_id = *self.screen.get(&Point { x, y }).unwrap_or(&0);
-                print!("{}", match tile_id {
-                    0 => ' ', // Empty
-                    1 => '#', // Wall
-                    2 => '▒', // Block
-                    3 => '─', // Paddle
-                    4 => 'o', // Ball
-                    _ => '?', // Unknown tile
-                });
+                print!("{}", Self::tile_id_to_char(tile_id));
             }
             println!();
         }
     }
+
+    fn tile_id_to_char(tile_id: u8) -> char {
+        match tile_id {
+            0 => ' ', // Empty
+            1 => '#', // Wall
+            2 => '▒', // Block
+            3 => '─', // Paddle
+            4 => 'o', // Ball
+            _ => '?', // Unknown tile
+        }
+    }
 }
 
-impl IOModule for ArcadeIOModule {
+impl IOModule for ArcadeIOModule<'_> {
     fn next_input(&mut self) -> Option<i64> {
+        self.first_input_requested = true;
         Some(self.joystick_state as i64)
     }
 
@@ -96,23 +127,61 @@ impl IOModule for ArcadeIOModule {
             if x == -1 && y == 0 {
                 // Special case for the score output
                 self.score = self.output_buffer[2] as u32;
+                self.score_history.push((self.score, self.count_tiles(2)));
                 if self.verbose {
-                    println!("Updated score: {}, {} tiles left", self.score, self.count_tiles(2));
+                    println!(
+                        "Updated score: {}, {} tiles left",
+                        self.score,
+                        self.count_tiles(2)
+                    );
+                } else if let Some(stdout) = self.stdout.as_mut() {
+                    stdout.queue(cursor::MoveTo(0, 30)).unwrap();
+                    stdout
+                        .queue(style::Print(format!("Score: {}", self.score)))
+                        .unwrap();
                 }
             } else {
                 let tile_id = self.output_buffer[2] as u8;
                 self.screen.insert(Point { x, y }, tile_id);
                 if self.verbose && tile_id == 4 {
                     println!("Ball at ({}, {})", x, y);
-                }
+                } else if let Some(stdout) = self.stdout.as_mut()
+                    && x >= 0
+                    && y >= 0
+                {
+                    stdout.queue(cursor::MoveTo(x as u16, y as u16)).unwrap();
+                    stdout
+                        .queue(style::Print(Self::tile_id_to_char(tile_id)))
+                        .unwrap();
 
+                    if (self.first_input_requested) {
+                        stdout.flush().unwrap();
+                        if poll(time::Duration::from_millis(1000 / 10)).unwrap() {
+                            // It's guaranteed that the `read()` won't block when the `poll()`
+                            // function returns `true`
+                            self.joystick_state = match read().unwrap() {
+                                Event::Key(event) => match event.code {
+                                    KeyCode::Left => JoystickState::Left,
+                                    KeyCode::Right => JoystickState::Right,
+                                    // Keep the current joystick state if an unexpected key
+                                    _ => self.joystick_state,
+                                },
+                                // Keep the current joystick state if a different event
+                                _ => self.joystick_state
+                            }
+                        } else {
+                            // If no input was given, switch back to neutral
+                            self.joystick_state = JoystickState::Neutral;
+                        }
+                    }
+                }
             }
             self.output_index = 0;
         }
     }
 }
 
-pub fn run(input_filename: &str) -> std::io::Result<()> {
+pub fn run(input_filename: &str) -> io::Result<()> {
     let mut file = std::fs::File::open(input_filename)?;
 
     // read file into a string
@@ -121,7 +190,7 @@ pub fn run(input_filename: &str) -> std::io::Result<()> {
 
     // part 1
     let memory = IntCode::parse_memory(&contents);
-    let mut arcade_io = ArcadeIOModule::new(false);
+    let mut arcade_io = ArcadeIOModule::new(false, None);
 
     let mut intcode = IntCode::new(memory, String::from("day13"), 0);
     intcode.run(Some(&mut arcade_io));
@@ -132,11 +201,15 @@ pub fn run(input_filename: &str) -> std::io::Result<()> {
     // part 2
     let mut memory = IntCode::parse_memory(&contents);
     memory[0] = 2;
-    let mut arcade_io = ArcadeIOModule::new(true);
+    let mut stdout = io::stdout();
+
+    let mut arcade_io = ArcadeIOModule::new(false, Some(&mut stdout));
 
     let mut intcode = IntCode::new(memory, String::from("day13"), 0);
     intcode.run(Some(&mut arcade_io));
-    println!("{:?}", intcode.state());
+    terminal::disable_raw_mode()?;
+
+    println!("{:?}", arcade_io.score_history());
 
     Ok(())
 }
